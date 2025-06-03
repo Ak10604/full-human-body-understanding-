8f58e8e4a5859cfd66b6786bcbf6d3872610210d
Ayush kaushik <sunny14kaushik6@gmail.com>
2025-06-03 14:09:53 +0530

    human body.py

--- human body.py
+++ human body.py
@@ -1,12 +1,9 @@
 import cv2
 import mediapipe as mp
 
-# Initialize MediaPipe pose class.
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
 mp_draw = mp.solutions.drawing_utils
-
-# Open the webcam.
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -14,16 +11,14 @@ while True:
     if not ret:
         break
 
-    # Convert the image to RGB.
     img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = pose.process(img_rgb)
 
-    # Draw landmarks.
     if results.pose_landmarks:
         mp_draw.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
     cv2.imshow("Human Body Structure Recognition", frame)
-    if cv2.waitKey(1) & 0xFF == 27:  # ESC to exit
+    if cv2.waitKey(1) & 0xFF == 27:
         break
 
 cap.release()
